4fe3c211a02f40d78c1b5ef94a9db7ea8798bc76
Johnny <johnnie.tseng@gmail.com>
2020-10-13 11:47:56 +0800

    Initial commit

---
+++ FBDemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FBDemoProject
+//
+//  Created by Johnny on 2020/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FBDemoProject/Pods-FBDemoProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FBDemoProject : NSObject
+@end
+@implementation PodsDummy_Pods_FBDemoProject
+@end
